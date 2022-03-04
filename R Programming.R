f2cb2f36465a7760d91c1bb744757b9953570d1a
Alida Pikstra <aampikstra@gmail.com>
2022-03-04 22:25:54 +0100

    Visualization with GGPlot2; Using Facets

--- R Programming.R
+++ R Programming.R
@@ -623,3 +623,38 @@ u + geom_boxplot(size=1.2)
 u + geom_boxplot(size=1.2) + geom_point()
 u + geom_boxplot(size=1.2) + geom_jitter()
 u + geom_jitter() + geom_boxplot(size=1.2, alpha=0.5)
+
+library(ggplot2)
+
+#---------- Using Facets
+v <- ggplot(data=Movie_Ratings, aes(x=Budgetmillions))
+v + geom_histogram(binwidth=10, aes(fill=Genre),
+                   colour="Black")
+
+#facets
+v + geom_histogram(binwidth=10, aes(fill=Genre),
+                   colour="Black") + 
+  facet_grid(Genre~., scales="free")
+
+#scatterplotsÑ
+w <- ggplot(data=Movie_Ratings, aes(x=CriticRating, y=AudienceRating,
+                                    colour=Genre))
+w + geom_point(size=3)
+
+#facets
+w + geom_point(size=3) +
+  facet_grid(Genre~.)
+
+w + geom_point(size=3) +
+  facet_grid(.~Year)
+
+w + geom_point(size=3) +
+  facet_grid(Genre~Year)
+
+w + geom_point(size=2) +
+  geom_smooth() +
+  facet_grid(Genre~Year)
+
+w + geom_point(aes(size=Budgetmillions)) +
+  geom_smooth() +
+  facet_grid(Genre~Year)
